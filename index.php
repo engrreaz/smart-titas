4551d95067745cb1193d4057f2ab93e4e2067eea
Reazul Hoque <engrreaz@gmail.com>
2026-06-03 05:22:59 +0600

    Commit to push

--- index.php
+++ index.php
@@ -233,7 +233,7 @@ $total_members_count = array_sum(array_column($friends, 'total_member'));
         <!-- Filter Options -->
         <div class="d-flex flex-wrap justify-content-center filter-nav mb-3">
             <?php foreach($points as $point): ?>
-                <a href="?pick_point=<?= urlencode($point) ?>" class="nav-link text-decoration-none <?= $filter_pick_point == $point ? 'active' : '' ?>" ‍style="font-size: 0.5rem;">
+                <a href="?pick_point=<?= urlencode($point) ?>" class="nav-link text-decoration-none <?= $filter_pick_point == $point ? 'active' : '' ?>" ‍style="font-size: 9px;">
                     <?= $point ?>
                 </a>
             <?php endforeach; ?>
